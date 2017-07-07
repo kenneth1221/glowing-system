08d1822f9fea8ba3fc5f0f6e025183c4c708d8e0
kenneth1221 <stevenxu@att.net>
2017-07-07 01:13:29 -0400

    More progress for the day, implementing more of the basic framework
    7/7/2017

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var godpower = 0
 var songs = 0
+var stars = 0
 var spirits = 0
+var oldspirits = 0
 var godtype = 'single'
 var spiritbonus = 1
 
@@ -29,9 +31,15 @@ function prettify(input){
 	var output = Math.round(input*1000000)/1000000;
 	return output;
 }
-
+function manualClick(n){
+	godClick(n);
+	if (godtype == 'duality'){
+		spirits = spirits + 1;
+		document.getElementById('spirits').innerHTML = spirits;
+	}
+}
 function godClick(n){
-	godpower = godpower + n;	
+	godpower = godpower + n;
 	document.getElementById('godpower').innerHTML = godpower;
 };
 
@@ -45,46 +53,110 @@ function makeSong(){
 	}
 	if(songs/10 >= spirits+1){
 		spirits = spirits + 1
-		document.getElementById('spirits').innerHTML = spirits;
-		companyCheck();
+		document.getElementById('spirits').innerHTML = spirits;;
 	}
 	var nextCost = Math.floor(10*Math.pow(1.1,songs));
 	document.getElementById('songCost').innerHTML = nextCost;
 	
 };
 
-function updateUpgrades(){
-	if (godtype == 'single'){
-		if (spirits >= 1) {
-			document.getElementById('duality').style.display = 'inline'
-		}
-		if (spirits >= 2) {
-
-		}
-		if (spirits >= 11){
-			
-		}
-	else if (godtype == 'pantheon'){
-		
+function makeStar(){
+	var starCost = Math.floor(100*Math.pow(1.1, songs));
+	if(godpower >= starCost){
+		stars = stars + 1;
+		godpower = godpower - starCost;
+		document.getElementById('stars').innerHTML = stars;
+		document.getElementById('godpower').innerHTML = godpower;
+		spirits = spirits + 1
+		document.getElementById('spirits').innerHTML = spirits;;
 	}
-	else if (godtype == 'duality'){
-		
-	}
-}
+	var nextCost = Math.floor(100*Math.pow(1.1,stars));
+	document.getElementById('starCost').innerHTML = nextCost;	
 }
 
-function companyCheck(){
+
+
+function changeGodType(newType){
+	godtype = newType;
+	messageChange(godtype);
+	document.getElementById('godtype').innerHTML = godtype;
+	var elements = document.getElementsByClassName('godtype')
+	for (var i = 0; i < elements.length; i++){
+        elements[i].style.display = 'none';
+    }
+}
+
+function messageChange(newGodtype){
+	if(newGodtype == 'duality'){
+		document.getElementById('status').innerHTML = 'You are alone, but you are also your own opposite.';
+		document.getElementById('spiritName').innerHTML = 'Sub-dualities';
+		document.getElementById('worshipName').innerHTML= 'Dance with your Opposite!';
+	}
+	if(newGodtype == 'pantheon'){
+		document.getElementById('status').innerHTML = 'You are one of a grand many.'
+		document.getElementById('spiritName').innerHTML = 'Gods';
+		document.getElementById('worshipName').innerHTML= 'Govern the Gods!';
+	}
+	if(newGodtype == 'henotheon'){
+		document.getElementById('status').innerHTML = 'King of Kings, Lord of Lords.'
+	}
+}
+function updateStatus(){
 	if(spirits >= 666){
 		document.getElementById('status').innerHTML = 'Beware the Beast!'
 	}		
-	else if(spirits >= 1){
+	else if(spirits >= 1 && godtype === 'single'){
 		document.getElementById('status').innerHTML = 'Are you still alone? Or is that second voice a figment of your imagination?'
 	}
 }
+function updateUpgrades(){
+	if (godtype === 'single'){
+		if (spirits >= 1) {
+			document.getElementById('duality').style.display = 'inline-block';
+		}
+		if (spirits >= 2) {
+			document.getElementById('trinity').style.display = 'inline-block';
+		}
+		if (spirits >= 11){
+			document.getElementById('pantheon').style.display = 'inline-block';
+		}
+	}
+	
+	if (godtype === 'pantheon'){
+		if (godpower > spirits*100){
+			document.getElementById('single').style.display = 'inline-block';
+		}
+		if (spirits > 100){
+			document.getElementById('henotheon').style.display = 'inline-block';
+		}
+	}
+	
+	if (godtype === 'duality'){
+		if (spirits >= 90){
+			document.getElementById('animist').style.display = 'inline-block';
+		}
+		if (spirits >= 1000){
+			document.getElementById('single').style.display = 'inline-block';
+		}
+	}
+}
 
+function updateSpiritBonus(){
+	if(godtype == 'duality'){
+		spiritbonus = spirits;
+	}
+}
+
+function updateAll(){
+	updateStatus();
+	updateUpgrades();
+	updateSpiritBonus();
+}
 
 window.setInterval(function(){
 	godClick(songs);
+	godClick(3*stars)
 	godClick(spiritbonus*spirits);
-	updateUpgrades();
+	updateAll();
+	
 }, 1000);
